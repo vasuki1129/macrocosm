9f1616f34ac97dd6d61ba7e018d412e12e0e23fb
Deepfrog <froggy@deepfrog.net>
2025-09-25 23:53:54 -0400

    SLAMMING!

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,28 @@
 
+//so like this is what dis shit actually is
+//the lua bit is just a frontend.
+//
+//this bit reads and analyzes a
+//(currently) c++ codebase
+//and analyzes it to generate
+//the file that the frontend
+//uses to render the visuals
+//
+//its gonna make sense i swear
+//
+//anybody that wanna write parsers
+//for other languages is free to
+//do so
+//
+
+#include <string>
+
+std::string getExtension(std::string str)
+{
+    return str.substr(str.find_last_of(".")+1);
+}
+
 int main(int argc, char *argv[])
 {
 
-    return 0;
 }
--- src/main.lua
+++ src/main.lua
@@ -1,3 +1,8 @@
 function love.draw()
     love.graphics.print('Hello World!', 400,300);
 end
+
+function love.load()
+
+
+end
